033622c1fb3f54f51d4024a05be1e866b4b6d0dd
Andrew Nesbitt <andrewnez@gmail.com>
2021-10-19 17:10:48 +0100

    Activate on first page load or after pjax loads

--- octobox.js
+++ octobox.js
@@ -3,10 +3,28 @@
 // TODO support github enterprise
 // TODO support self-hosted octobox instances
 
-// 1. check if loaded on an individual issue or pull request page
+function isActionablePage() {
+  // check if loaded on an individual issue or pull request page
+  //   *owner*/*name*/issues/*number*
+  //   *owner*/*name*/pull/*number*
 
-  // *owner*/*name*/issues/*number*
-  // *owner*/*name*/pull/*number*
+  var parts = window.location.pathname.split('/');
+  return parts.length > 4 && ['issues', 'pull'].includes(parts[3])
+}
+
+function activate() {
+  if(isActionablePage()){
+    console.log('Octobox: Activate!!!')
+  }
+}
+
+// load on first page load
+activate()
+
+// load again after a pjax load
+document.addEventListener('pjax:end', () => {
+  activate()
+});
 
 // 2. check if authenticated with octobox
 
@@ -15,7 +33,8 @@
 
 // 3. attempt to load notification for current issue/pr from octobox.io
 
-  // TODO need an Octobox endpoint to load notification via subject_url
+  // authenticated json request to https://octobox.io/notifications/lookup?url=*issueorpullurl*
+
 
 // 4. display octobox action bar
 
